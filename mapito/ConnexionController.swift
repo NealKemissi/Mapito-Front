df0a915176448717d0e4cefad1a7cd4c835154a3
m2sar <m2sar@eduroam420.canalip.upmc.fr>
2018-04-12 16:59:15 +0200

    [develop] Created architecture of NavigationController and Tab Bar Controller

---
+++ mapito/ConnexionController.swift
@@ -0,0 +1,22 @@
+//
+//  ConnexionController.swift
+//  mapito
+//
+//  Created by m2sar on 12/04/2018.
+//  Copyright © 2018 code2geny. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConnexionController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
