db0ecf0e1331e8636385b56b7b7fd76273ce03de
m2sar <m2sar@client-eduroam301.canalip.upmc.fr>
2018-04-21 15:32:45 +0200

    table view profile+modif profile

---
+++ mapito/TableCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableCell.swift
+//  mapito
+//
+//  Created by m2sar on 21/04/2018.
+//  Copyright © 2018 code2geny. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableCell : UITableViewCell {
+    
+    @IBOutlet weak var Champs: UILabel!
+    var champsArray = ["Modifier Nom","Modifier Prénom"]
+}
