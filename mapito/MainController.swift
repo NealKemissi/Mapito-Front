3f5ecd69c49c753ab0a23cdaf07d24b7419a760b
Heloise-Chauvel <chauvel.h@gmail.com>
2018-06-06 20:27:28 +0200

    final merge

--- mapito/MainController.swift
+++ mapito/MainController.swift
@@ -14,6 +14,7 @@ class MainController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    //test
     
     override func viewDidAppear(_ animated: Bool) {
         let defaults = UserDefaults.standard;
